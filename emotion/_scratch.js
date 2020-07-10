07a2cfe6b0e62ccf71a5d48713a0ce8241b9487e
Lu Nelson <lunelson@gmail.com>
2020-07-10 13:57:37 +0200

    bring in the other emotion 11 packages, use the serializer correctly but also keep serlializing the global CSS

--- emotion/_scratch.js
+++ emotion/_scratch.js
@@ -4,13 +4,14 @@
   babel: {
     presets: [
       "@babel/preset-env",
-      "@babel/preset-react"
+      "@babel/preset-react",
+      "@emotion/babel-preset-css-prop",
     ],
     plugins: [
       [
         "@emotion/babel-plugin",
         {
-          autoLabel: "never",
+          autoLabel: "always",
         }
       ],
       // require('../test-babel-plugin'),
@@ -21,29 +22,34 @@
   ]
 });
 
-/** @jsx jsx */
 import React from 'react';
 import { render, prettyDOM } from '@testing-library/react';
 import prettier from 'prettier';
-import { Global, css, jsx } from '@emotion/react';
+import { Global, css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { createSerializer } from '@emotion/jest';
 
 function debug(component) {
 
-  const container = render(
-    <>
-      <Global styles={globalStyles} />
-      <div css={[base, danger]} >
-        <MyBox css={css`color: green;`} />
-      </div>
-    </>
-  ).container;
+  const serializer = createSerializer();
+  const container = render(component).container;
+
+  console.log(`
+--------------------------------------------------------------------------------
+
+${prettier.format(container.ownerDocument.styleSheets[container.ownerDocument.styleSheets.length - 1].toString(), { parser: 'css'})}
+${serializer.serialize(container, {indent: '  '}, '  ', Infinity, null, (html) => prettyDOM(html))}
+  `);
+}
+
+function debug2(component) {
+
+  const container = render(component).container;
 
   console.log(`
 --------------------------------------------------------------------------------
 
 ${prettier.format(container.ownerDocument.styleSheets.map(sheet => sheet.toString()).join('\n'), { parser: 'css'})}
-
 ${prettyDOM(container)}
   `);
 }
@@ -52,37 +58,48 @@ ${prettyDOM(container)}
 
 const MyBox = styled.div`
   color: red;
-  height: 200px;
+  height: 500px;
 `;
 
-const what = css`
-  display: grid;
-  transition: all 0.5s;
-  user-select: none;
-  background: linear-gradient(to bottom, white, black);
+const mqtest = css`
+  ${css`padding-top: 20px; background-color: yellow;`}
+  ${[
+    '@media screen {',
+      css`margin-left: 100px;`,
+    '}'
+  ]}
 `;
+
+const stackBase = css`
+  & > * + * { margin-top: var(--stack-gap); }
+`;
+
+const stack = gap => [stackBase, css`--stack-gap: ${gap};`]
 
 const danger = css`
   color: orange;
 `
 
 const base = css`
-  color: blue;
+  color: forestgreen;
 `
 
 const globalStyles = css`
   body {
-    color: blue;
+    color: black;
+    line-height: 1.4;
   }
 `;
 
 debug(
   <>
     <Global styles={globalStyles} />
-    <div css={what}>
+    <div css={stack(20)}>this is 20</div>
+    <div css={stack(40)}>this is 40</div>
+    <div css={[mqtest]}>
       <p>is this thing on?</p>
     </div>
-    <div css={[base, what]} >
+    <div css={[base, danger]} >
       <MyBox css={css`color: green;`} />
     </div>
   </>
