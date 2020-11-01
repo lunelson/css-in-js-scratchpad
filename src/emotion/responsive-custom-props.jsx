5f30155205f89293b326a6e138969ac9fcf1f69a
Lu Nelson <lunelson@gmail.com>
2020-11-01 13:21:55 +0100

    doodle

--- src/emotion/responsive-custom-props.jsx
+++ src/emotion/responsive-custom-props.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import { css } from '@emotion/css';
 import cn from 'classnames';
 
+const Box = ({ as = 'div', mt, mb, span, rowSpan, children, ...restProps }) => {
+  const classes = [
+    // mt && ['mt', css`--mt: ${mt};`],
+    // mb && ['mb', css`--mb: ${mb};`],
+    span && ['span', css`--column-span: ${span};`],
+    rowSpan && ['row-span', css`--row-pan: ${rowSpan};`],
+  ].filter(Boolean)
+  return React.createElement(as, { className: cn(...classes), ...restProps }, children)
+}
+
 export const Test = () => (
   <>
+    <Box mb={30} span={2} rowSpan={2}></Box>
+    <Box as='span' mb={30} mt={10}></Box>
   </>
 )
